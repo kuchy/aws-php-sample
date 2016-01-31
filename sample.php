165761c24b4ca4945a76d8f1e586950e41b7f88e
Martin Kuchyňár <kuchynar@sven.sk>
2016-01-31 11:58:14 +0100

    Rewriting sample to simple protory to cache images in amazon s3 services.

--- sample.php
+++ sample.php
@@ -30,7 +30,7 @@ require 'vendor/autoload.php';
 */
 $s3 = new Aws\S3\S3Client([
     'version' => '2006-03-01',
-    'region'  => 'us-west-2'
+    'region'  => 'eu-central-1'
 ]);
 
 /*
@@ -40,12 +40,12 @@ $s3 = new Aws\S3\S3Client([
  For more information about bucket name restrictions, see:
  http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
 */
-$bucket = uniqid("php-sdk-sample-", true);
-echo "Creating bucket named {$bucket}\n";
-$s3->createBucket(['Bucket' => $bucket]);
+$bucket = 'kuchy';
+$filename  ='orange-pi-one-pocitac-raspberry-pi-nestandard2.jpg';
+$imgUrl = 'http://ipravda.sk/res/2016/01/30/thumbs/'.$filename;
+$img = file_get_contents($imgUrl);
 
-// Wait until the bucket is created
-$s3->waitUntil('BucketExists', ['Bucket' => $bucket]);
+
 
 /*
  Files in Amazon S3 are called "objects" and are stored in buckets. A specific
@@ -55,88 +55,26 @@ $s3->waitUntil('BucketExists', ['Bucket' => $bucket]);
  For a detailed list of putObject's parameters, see:
  http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-s3-2006-03-01.html#putobject
 */
-$key = 'hello_world.txt';
-echo "Creating a new object with key {$key}\n";
-$s3->putObject([
-    'Bucket' => $bucket,
-    'Key'    => $key,
-    'Body'   => "Hello World!"
-]);
 
-/*
- Now, let's download the object and read the body directly.
+$response = $s3->doesObjectExist($bucket, $filename);
 
- For more examples of downloading objects, see the developer guide:
- http://docs.aws.amazon.com/aws-sdk-php-2/guide/latest/service-s3.html#downloading-objects
+if ($response){
+  echo 'Image already exist!' . "\r\n";
+  echo 'url: https://s3.eu-central-1.amazonaws.com/kuchy/'.$filename . "\r\n";  
+}
+else{
+  echo "Creating a new object with key {$filename}\n";
+  $result =$s3->putObject([
+      'Bucket' => $bucket,
+      'Key'    => $filename,
+      'Body'   => $img,
+      'ContentType' => 'image/jpeg',
+      'ACL'          => 'public-read',
+  ]);
 
- Or the API documentation:
- http://docs.aws.amazon.com/aws-sdk-php-2/latest/class-Aws.S3.S3Client.html#_getObject
-*/
+  echo 'url: '.$result['ObjectURL'] . "\r\n";  
+}
 
-echo "Downloading that same object:\n";
-$result = $s3->getObject([
-    'Bucket' => $bucket,
-    'Key'    => $key
-]);
 
-echo "\n---BEGIN---\n";
-echo $result['Body'];
-echo "\n----END----\n\n";
 
-/*
- You can perform the same upload and download operations using Amazon S3 Stream Wrappers, 
- which allow you to store and retrieve data from Amazon S3 using PHP's built-in functions
- like file_get_contents, fopen, copy, etc.
 
- For more information about stream wrappers, see the Amazon S3 Stream Wrapper
- section in the developer guide:
- http://docs.aws.amazon.com/aws-sdk-php/v3/guide/service/s3-stream-wrapper.html
-*/
-
-// First, you need register the stream wrappers. 
-
-$s3->registerStreamWrapper();
-
-// Now you can use addresses such as s3://bucket/key as with built-in functions as shown below.
-
-$key2 = 'hello_again_world.txt';
-echo "Creating a second object with key {$key2} using stream wrappers\n";
-file_put_contents("s3://{$bucket}/{$key2}", 'Hello Again!');
-
-// Let's get the contents of this object.
-
-echo "Downloading that same object:\n";
-$data = file_get_contents("s3://{$bucket}/{$key2}");
-echo "\n---BEGIN---\n";
-echo $data;
-echo "\n----END----\n\n";
-
-/*
- Now, we want to delete the bucket. Buckets cannot be deleted unless they're empty. 
- With the AWS SDK for PHP, you have a few options when deleting multiple objects:
-
-  - Use deleteMatchingObjects method:
-      http://docs.aws.amazon.com/aws-sdk-php/v3/api/class-Aws.S3.S3Client.html#_deleteMatchingObjects 
-  - Use the BatchDelete helper:
-      http://docs.aws.amazon.com/aws-sdk-php/v3/api/class-Aws.S3.BatchDelete.html
-  - Or individually delete the objects.
-
- We'll use the BatchDelete helper to delete the two objects we created.
-*/
-echo "Deleting all objects in bucket {$bucket}\n";
-$batch = Aws\S3\BatchDelete::fromListObjects($s3, ['Bucket' => $bucket]);
-$batch->delete();
-
-/*
- Now that the bucket is empty, it can be deleted.
-*/
-
-echo "Deleting bucket {$bucket}\n";
-$s3->deleteBucket(['Bucket' => $bucket]);
-
-/*
- Although this sample didn't check for errors when calling service operations,
- real-world applications should always check for errors.
- See the Error Handling section in the developer guide for more information:
- http://docs.aws.amazon.com/aws-sdk-php/v3/guide/getting-started/basic-usage.html#synchronous-error-handling
-*/
